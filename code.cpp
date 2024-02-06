9f8c31ddf6d0bb06cdfa631f74d35663ad38d5e8
Abdulrahman Al Muaitah <a.almuaitah@gmail.com>
2024-02-06 23:15:50 +0400

    First version 1.0

---
+++ code.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+using namespace std; 
+int main(){
+    //This is the main function, it takes in two integer values and returns the summation of them 
+    int x,y; 
+    cout << "Please enter the value for x and y: \n"; 
+    cin >> x >> y; 
+    cout << "The summation of " << x << " and " << y << " is: " << x+y << "\n";
+    return 0;
+}
